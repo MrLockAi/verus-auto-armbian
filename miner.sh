b2bd9114ec50568c9c425cf19c925979fc151627
Irvan Tijani <irvan.tijani@gmail.com>
2021-10-29 00:22:26 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,1 +1,1 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u wallet.workername -p x -t 4
+/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956#xnsub -u RQyL5Vz7PYzxS9QsH2aeo9YFP7EhGwGpKj.001 -p hybrid -t 4
